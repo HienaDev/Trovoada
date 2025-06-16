2b5c2251e19894607aba9d63d11c6cc76168af6c
HienaDev <ant.rodrigues1999@gmail.com>
2025-06-16 16:33:35 +0100

    Add floor scenario

--- Assets/TeleportOnCollision.cs
+++ Assets/TeleportOnCollision.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TeleportOnCollision : MonoBehaviour
 {
     [SerializeField]
     private Transform teleportTarget;
+
+    [SerializeField]
+    private UnityEvent onTeleport;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -23,12 +27,19 @@ public class TeleportOnCollision : MonoBehaviour
             return;
         }
 
-        // Teleport position
+        // Teleport position and rotation
         obj.transform.position = teleportTarget.position;
-
-        // Teleport rotation
         obj.transform.rotation = teleportTarget.rotation;
 
+        // Reset rigidbody velocity if present
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
 
+        // Invoke UnityEvent
+        onTeleport?.Invoke();
     }
 }
